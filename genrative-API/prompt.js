5272997518a95efb69cbd9bb779f13adc0c7a81a
harshitsharma88 <harshitsrma88@gmail.com>
2024-07-17 19:00:26 +0530

    ..

--- genrative-API/prompt.js
+++ genrative-API/prompt.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI('AIzaSyBSHvMqJnUhJpbeMbV_KO5qnoKU5Ldx55U');
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
